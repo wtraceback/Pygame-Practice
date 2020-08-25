2533f4edf6a28b01b63ccfef268a23fb54a7ae47
whxcer <whxcer@outlook.com>
2020-08-25 21:11:02 +0800

    多条连续不间断的线段，终点也是新的线段的起点，点击右键可以停止划线

---
+++ Pygame小练习/连续不间断的线段.py
@@ -0,0 +1,68 @@
+import sys
+import pygame
+
+
+class Configuration():
+    """配置相关的类"""
+
+    def __init__(self):
+        self.size = (480, 700)
+        self.caption = 'Draw multiple lines with the mouse'
+        self.bg_color = (230, 230, 230)
+
+        # 线段相关
+        self.start = (0, 0)
+        self.end = (0, 0)
+        self.drawing = False
+        self.line_color = (255, 0, 0)
+        self.line_width = 2
+        self.lines = []
+
+
+def main():
+    configs = Configuration()
+
+    # 游戏的初始化、游戏窗口的设置、游戏窗口标题的设置
+    pygame.init()
+    screen = pygame.display.set_mode(configs.size)
+    pygame.display.set_caption(configs.caption)
+
+    while True:
+        for event in pygame.event.get():
+            # 关闭游戏窗口
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # 确定线段的起点
+                temp_line_coordinate = configs.start, configs.end
+                configs.lines.append(temp_line_coordinate)
+
+                configs.start = event.pos
+                configs.end = event.pos
+                configs.drawing = True
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                # 右键停止画线
+                if event.button == 3:
+                    configs.drawing = False
+
+                # 确定线段的终点
+                configs.end = event.pos
+
+            elif event.type == pygame.MOUSEMOTION and configs.drawing:
+                # 不断变化中的线段
+                configs.end = event.pos
+
+        screen.fill(configs.bg_color)
+
+        for line in configs.lines:
+            pygame.draw.line(screen, configs.line_color, line[0], line[1], configs.line_width)
+
+        # 绘制线段
+        pygame.draw.line(screen, configs.line_color, configs.start, configs.end, configs.line_width)
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
